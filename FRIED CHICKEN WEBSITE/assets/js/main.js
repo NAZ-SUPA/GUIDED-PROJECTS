becc7c06df8f6afb1b46c39b7864f5e497808173
NAZSUPA <Naz.feng0438@koyauniversity.org>
2026-08-14 03:02:29 +0300

    end.

--- FRIED CHICKEN WEBSITE/assets/js/main.js
+++ FRIED CHICKEN WEBSITE/assets/js/main.js
@@ -111,7 +111,6 @@ window.addEventListener('scroll', scrollUp);
  * - This gives users visual feedback of which page section they are currently
  *   viewing, highlighting the corresponding nav item.
  */
-
 const sections = document.querySelectorAll('section[id]');
 
 const scrollActive = () => {
@@ -119,20 +118,55 @@ const scrollActive = () => {
 
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 58,
-            sectionId = current.getAttribute('id'),
-            sectionClass = document.querySelector(
-                '.nav__menu a[href*=' + sectionId + ']'
-            );
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id');
+        
+         sectionClass = document.querySelector('.nav__menu a[href*=' +sectionId + ']');
 
-        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-            sectionClass.classList.add('active-link');
-        } else {
-            sectionClass.classList.remove('active-link');
+        if (sectionClass) {
+            if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+                sectionClass.classList.add('active-link');
+            } else {
+                sectionClass.classList.remove('active-link');
+            }
         }
     });
 };
+
 window.addEventListener('scroll', scrollActive);
 
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 300,
+    // reset: true, // Animation repeat
+})
+
+sr.reveal('.home__data, .about__data , .footer__container')
+sr.reveal('.home__images', {delay:1000})
+sr.reveal('.home__dam-1', {delay:1000, scale:0, rotate:{z:45}})
+sr.reveal('.home__dam-2, .home__dam-3', {delay:1400, scale:0, rotate:{z:45}})
+sr.reveal('.home__dam-4', {delay:1700, scale:0, rotate:{z:45}})
+sr.reveal('.home__dam-5', {delay:1800, scale:0, rotate:{z:45}})
+sr.reveal('.about__image-1', {delay:600, origin:'right'})
+sr.reveal('.about__image-2', {delay:800, origin:'left'})
+sr.reveal('.about__tooltip-1', {delay:1600, origin:'left'})
+sr.reveal('.about__tooltip-2', {delay:1800, origin:'right'})
+sr.reveal('.order__card:nth-child(1)', {origin:'right'})
+sr.reveal('.order__card:nth-child(2)', {origin:'left'})
+sr.reveal('.order__tooltip-1', {delay:800, origin:'right'})
+sr.reveal('.order__tooltip-2', {delay:800, origin:'left'})
+sr.reveal('.combo__titles', {origin:'bottom'})
+sr.reveal('.combo__img', {delay:1000})
+sr.reveal('.combo__data', {delay:1600})
+sr.reveal('.combo__numbers', {delay:1600, origin:'bottom'})
+sr.reveal('.contact__data', {origin:'right'})
+sr.reveal('.contact__info', {origin:'left'})
+sr.reveal('.contact__img-1',{delay:1000, distance:0, scale:0, rotate:{z:-45}})
+sr.reveal('.contact__img-2',{delay:1200, distance:0, scale:0, rotate:{z:45}})
+sr.reveal('.contact__dam-1',{delay:1400,  scale:0, rotate:{z:45}})
+sr.reveal('.contact__dam-3',{delay:1600,  scale:0, rotate:{z:45}})
+sr.reveal('.contact__dam-2',{delay:1800,  scale:0, rotate:{z:45}})
+sr.reveal('.contact__dam-4',{delay:2000,  scale:0, rotate:{z:45}})
